8c96d3091864515bb8ca9159c726da90702751a1
ahmethedev <ahmetburakdincc@gmail.com>
2024-01-21 17:34:56 +0300

    Day2 - Responsive UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from 'next/head'
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThirdwebProvider activeChain="ethereum" clientId="ChainId.Rinkeby">
       <Head>
         <title>NFT Marketplace</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThirdwebProvider>  
   )
 }
